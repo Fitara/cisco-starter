b01a206e856ed22e920df069395bf4d1311e91e1
unknown <justfitra11@gmail.com>
2023-10-25 20:35:19 +0700

    fix: latency address, styles: font

--- sextant/src/components/IpAddress.tsx
+++ sextant/src/components/IpAddress.tsx
@@ -1,40 +1,49 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 interface IpProps {
   version: string;
 }
 
 export default function IpAddress({ version }: IpProps) {
-    const [ipAddress, setIpAddress] = useState<string>('');
-    const [loading, setLoading] = useState<boolean>(true);
+  const [ipAddress, setIpAddress] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchIpAddress = async () => {
       try {
-        const response = await axios.get(`
-          https://api.ipify.org?format=${version}`
+        const response = await axios.get(
+          `https://api.ipify.org?format=${version}`
         );
-          setIpAddress(response.data);
+        const ipAddress = response.data;
+
+        if (version === "v4" && ipAddress.includes(".")) {
+          setIpAddress(ipAddress);
+        } else if (version === "v6" && ipAddress.includes(":")) {
+          setIpAddress(ipAddress);
+        } else {
+          setIpAddress("N/A");
+        }
       } catch (error) {
-          console.error('Error fetching IP address:', error);
+        console.error("Error fetching IP address:", error);
+        setIpAddress("Error");
       }
-      setLoading(false)
+      setLoading(false);
     };
 
     fetchIpAddress();
   }, [version]);
 
   return (
-    <div className="ip-container">
+    <div className='ip-container'>
       {loading ? (
-        <div className="loading-wrapper">
-          <div className="loading"></div>
+        <div className='loading-wrapper'>
+          <div className='loading'></div>
         </div>
       ) : (
-        <div className="ip-content">
-          <p className="ip-label">Public IP Address ({version}) :</p>
-          <p className="ip-address">{ipAddress}</p>
+        <div className='ip-content'>
+          <p className='ip-label'>Public IP Address ({version}) :</p>
+          <p className='ip-address'>{ipAddress}</p>
         </div>
       )}
     </div>
